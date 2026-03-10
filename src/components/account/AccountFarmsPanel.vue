5c6e620e6b74d2763d36fb5c1317e50551776b0c
Andrew Berry <andberry89@gmail.com>
2026-03-10 16:11:51 -0400

    feat(account): add owner badge to farms in account panel

--- src/components/account/AccountFarmsPanel.vue
+++ src/components/account/AccountFarmsPanel.vue
@@ -8,10 +8,18 @@
         :key="farm.id"
         class="bg-amber-50 border-menu rounded-lg p-4 space-y-3"
       >
-        <div class="font-stardew-bold text-green-950">
-          {{ farm.name }}
-        </div>
+        <div class="flex items-center gap-2">
+          <div class="font-stardew-bold text-green-950 leading-none">
+            {{ farm.name }}
+          </div>
 
+          <span
+            v-if="farm.created_by === currentUserId"
+            class="border-item grad-amber px-2 py-1 text-md leading-none font-quicksand text-orange-950 relative -top-px"
+          >
+            Owner
+          </span>
+        </div>
         <div class="text-md text-gray-600">Code: {{ farm.code }}</div>
 
         <button
